b9f7650af490c0a6333d823ead5accb3fbd32b7c
unknown <joaco.cataldo3@gmail.com>
2022-06-30 23:25:06 -0400

    subiendo estructuras de carpetas y home.html

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
-const path = ("path");
+const path = require("path");
+
+const publicPath = path.join(__dirname, "./public");
+
+app.use(express.static(publicPath));
 
 app.listen(3030, () => {
     console.log("Servidor levantado en 3030")
 })
 
+app.get ("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
